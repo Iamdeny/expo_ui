2311d824a6320f9940955b7eae69120635a51603
hellodeny <denchakuznec@gmail.com>
2026-06-28 21:06:24 +0900

    Новый заголовок для тестирования S8

--- src/screens/index.tsx
+++ src/screens/index.tsx
@@ -61,7 +61,7 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Device Memory Info (OTA test S8+)</Text>
+      <Text style={styles.title}>Device Memory Info (OTA test S8)</Text>
       <Text style={styles.subtitle}>via Native Module (ReactPackage)</Text>
 
       {/* Диагностическая строка */}
